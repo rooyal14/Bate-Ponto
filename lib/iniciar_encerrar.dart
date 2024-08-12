2faef505ad09ca0c1ce370197b042979258c2aa9
Rodrigo <zwi.rodrigo@gmail.com>
2024-08-12 03:36:32 -0400

    Implementar Tempo Restante e função de acelerar tempo passado

--- lib/iniciar_encerrar.dart
+++ lib/iniciar_encerrar.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ExpedientePage extends StatefulWidget {
@@ -16,29 +20,78 @@ class _ExpedientePageState extends State<ExpedientePage> {
 
   String? iniciarTime;
   String? encerrarTime;
+  Duration? dailyGoal;
   Duration? workedDuration;
+  Duration? remainingTime;
 
   DateTime? startTime;
   DateTime? endTime;
 
+  Timer? _timer;
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   void _toggleButtons() {
     setState(() {
-      if (isIniciarEnabled) {
-        // Store the start time and display it
-        startTime = DateTime.now();
-        iniciarTime = '${startTime!.hour.toString().padLeft(2, '0')}:${startTime!.minute.toString().padLeft(2, '0')}';
-      } else {
-        // Store the end time and calculate the duration
-        endTime = DateTime.now();
-        encerrarTime = '${endTime!.hour.toString().padLeft(2, '0')}:${endTime!.minute.toString().padLeft(2, '0')}';
-        workedDuration = endTime!.difference(startTime!);
-      }
-
       // Toggle the button states
       isIniciarEnabled = !isIniciarEnabled;
       isEncerrarEnabled = !isEncerrarEnabled;
     });
   }
+
+
+  void _handleIniciar(){
+    setState(() {
+      // Store the start time and display it
+      startTime = DateTime.now();
+      _artificialTimePassingLoop(startTime, 1);
+      iniciarTime = '${startTime!.hour.toString().padLeft(2, '0')}:${startTime!.minute.toString().padLeft(2, '0')}';
+      workedDuration = Duration(hours: 0,minutes: 0);
+      _toggleButtons();
+    });
+
+  }
+
+  void _handleEncerrar(){
+    setState(() {
+      _toggleButtons();
+    });
+
+  }
+
+  //Função responsável pela passagem de tempo de acordo com um ponto inicial e um multiplicador de velocidade
+  void _artificialTimePassingLoop(DateTime? artificiallySelectedTime, int speedUp) {
+    DateTime? now = artificiallySelectedTime;
+    _timer = Timer.periodic(Duration(seconds: 2), (Timer timer) {
+      now = now!.add(Duration(seconds: (2*speedUp)));
+      setState(() {
+        //Atualiza Total de horas trabalhadas até agora
+        workedDuration = now!.difference(startTime!);
+        //remainingTime = workedDuration.difference(dailyGoal!);
+        remainingTime = dailyGoal!-workedDuration!;
+      });
+
+
+      //EncerrarClickado
+      if(!isEncerrarEnabled) {
+        setState(() {
+          //Seta endTime para agora
+          endTime = now;
+          //Formata para exibição
+          encerrarTime = '${endTime!.hour.toString().padLeft(2, '0')}:${endTime!.minute.toString().padLeft(2, '0')}';
+        });
+        //Mata timer
+        timer.cancel();
+      }
+    });
+  }
+
+
+
 
   String _formatDuration(Duration duration) {
     final hours = duration.inHours;
@@ -58,6 +111,12 @@ class _ExpedientePageState extends State<ExpedientePage> {
       hours: widget.endHours.hour,
       minutes: widget.endHours.minute,
     );
+    setState(() {
+      dailyGoal = Duration(
+        hours: widget.workHours.hour,
+        minutes: widget.workHours.minute,
+      );
+    });
 
     return Scaffold(
       appBar: AppBar(
@@ -79,6 +138,12 @@ class _ExpedientePageState extends State<ExpedientePage> {
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.red),
               textAlign: TextAlign.center,
             ),
+            SizedBox(height: 10),
+            Text(
+              remainingTime != null ? 'Horário restante: ${_formatDuration(remainingTime!)}' : '',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.brown),
+              textAlign: TextAlign.center,
+            ),
             SizedBox(height: 40),
             // Create a grid layout with two columns
             Row(
@@ -97,7 +162,7 @@ class _ExpedientePageState extends State<ExpedientePage> {
                         ),
                       ),
                       ElevatedButton(
-                        onPressed: isIniciarEnabled ? _toggleButtons : null,
+                        onPressed: isIniciarEnabled ? _handleIniciar : null,
                         child: Text('Iniciar Expediente'),
                       ),
                     ],
@@ -117,7 +182,7 @@ class _ExpedientePageState extends State<ExpedientePage> {
                         ),
                       ),
                       ElevatedButton(
-                        onPressed: isEncerrarEnabled ? _toggleButtons : null,
+                        onPressed: isEncerrarEnabled ? _handleEncerrar : null,
                         child: Text('Encerrar Expediente'),
                       ),
                     ],
